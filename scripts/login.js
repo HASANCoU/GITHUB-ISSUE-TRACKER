294668ec91a4ce2704da5caa749fed956ef9c9a4
HASANCoU <mehedi.hasancou2@gmail.com>
2026-03-07 09:26:54 +0600

    Login Page Designed and Functionality done

--- scripts/login.js
+++ scripts/login.js
@@ -1,1 +1,25 @@
 console.log("Login.js Connected With index.html");
+
+const passwordInput = document.getElementById('password');
+const usernameInput = document.getElementById('username');
+const btn = document.getElementById('signin-btn');
+
+
+
+btn.addEventListener("click",()=>{
+
+    const password = passwordInput.value;
+    const username = usernameInput.value;
+
+    console.log(password,username);
+
+    if(username==="admin" && password === "admin123"){
+        alert("Login Successful");
+        window.location.assign('../home.html');
+    }
+    else{
+        
+        alert("Invalid Credentials!!!")
+    }
+})
+
